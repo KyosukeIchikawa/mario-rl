500046564d8478fd70b73074f00c2176f6f63995
KyosukeIchikawa <kyosuke.ichikawa@groove-x.com>
2023-09-22 03:01:39 +0900

    Add test for weighted_sample_indices_for_spiky_values()

--- tests/rl_util/test_prioritized_replay_buffer.py
+++ tests/rl_util/test_prioritized_replay_buffer.py
@@ -30,3 +30,20 @@ def test_sum_tree_weighted_sample_index(random_value, expected_index):
     with patch('random.uniform', return_value=random_value):
         index = sum_tree.weighted_sample_index()
     assert index == expected_index
+
+
+@pytest.mark.parametrize("random_values, expected_indices", [
+    [[0], [0]],
+    [[0] * 10, [0] * 10],
+    [[0, 4, 4.1, 5, 5, 5.1, 105, 106], [0, 0, 1, 1, 1, 2, 2, 3]],
+])
+def test_sum_tree_weighted_sample_indices_for_spiky_values(random_values, expected_indices):
+    sum_tree = _SumTree(capacity=4)
+    sum_tree[0] = 4
+    sum_tree[1] = 1
+    sum_tree[2] = 100
+    sum_tree[3] = 1
+
+    sum_tree._get_sorted_random_values = lambda _: random_values
+    index = sum_tree.weighted_sample_indices_for_spiky_values(len(random_values))
+    assert index == expected_indices
