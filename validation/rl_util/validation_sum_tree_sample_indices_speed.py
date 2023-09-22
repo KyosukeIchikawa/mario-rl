d5c9dc31bfdfcd04acf342d25c1d5ab65ee78deb
KyosukeIchikawa <kyosuke.ichikawa@groove-x.com>
2023-09-22 23:45:23 +0900

    Refactor validation

--- validation/rl_util/validation_sum_tree_sample_indices_speed.py
+++ validation/rl_util/validation_sum_tree_sample_indices_speed.py
@@ -1,10 +1,4 @@
-import sys
-import os
 import time
-
-current_directory = os.path.dirname(os.path.abspath(__file__))
-root_directory = os.path.abspath(os.path.join(current_directory, "..", ".."))
-sys.path.append(root_directory)
 
 import numpy as np
 
